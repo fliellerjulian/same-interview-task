52f3479076b9eca169bb29acc86ee7deb7570196
Julian Flieller <julianflieller@yahoo.de>
2025-06-05 20:29:29 -0700

    ExpandableCodeBlock back to monaco-editor/

--- src/components/ExpandableCodeBlock.tsx
+++ src/components/ExpandableCodeBlock.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Editor from "@monaco-editor/react";
 
 interface ExpandableCodeBlockProps {
   code: string;
@@ -54,28 +55,35 @@ export default function ExpandableCodeBlock({
           </button>
           <button
             onClick={onApply}
-            className="p-1 hover:bg-gray-700 rounded"
+            className="px-3 py-1 bg-blue-600 hover:bg-blue-700 text-white rounded transition-colors text-sm font-semibold"
             title="Apply changes"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
+            Apply
           </button>
         </div>
       </div>
       {isExpanded && (
-        <pre className="bg-gray-900 text-white p-4 rounded-b-lg overflow-x-auto">
-          <code>{code}</code>
-        </pre>
+        <div className="bg-gray-900 rounded-b-lg overflow-x-auto">
+          <Editor
+            height="200px"
+            width="100%"
+            defaultLanguage="javascript"
+            value={code}
+            theme="vs-dark"
+            options={{
+              fontSize: 14,
+              minimap: { enabled: false },
+              wordWrap: "on",
+              scrollBeyondLastLine: false,
+              automaticLayout: true,
+              readOnly: true,
+              lineNumbers: "on",
+              glyphMargin: true,
+              lineDecorationsWidth: 5,
+              lineNumbersMinChars: 3,
+            }}
+          />
+        </div>
       )}
     </div>
   );
